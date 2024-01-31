33266ddee471af6c5df7053ccfbcc7150cd7a8f7
gatsby2016 <gatsby2015@163.com>
2024-01-31 20:32:07 +0800

    update evaluation code

--- s5_Evaluation.py
+++ s5_Evaluation.py
@@ -110,6 +110,15 @@ def load_data(csv_path, label_mapping=None, filter_dict = None):
     return slide_ids, labels
 
 
+def load_data_cvfoldtest(csv_path, filename="", label_mapping=None):
+    data = pd.read_csv(os.path.join(csv_path, filename))
+    
+    slide_ids = data['test'].dropna().reset_index(drop=True)
+
+    labels = [label_mapping[slide.split('_')[0].split('Her2')[-1]] for slide in slide_ids]
+    return slide_ids, labels
+
+
 def load_model(params_dict, num_cluster, trained_pt_loc=None, fold=0, device=None):
     model = _init_model(params_dict["model_type"], params_dict["model_size"], params_dict["encoding_dim"], 
                         params_dict["drop_out"], params_dict["n_classes"], params_dict["top_num_inst"], 
@@ -135,26 +144,39 @@ def read_params_file(filename):
 
 if __name__ == "__main__":
     # params on trained dataset 
+    # trained_dataset_name = "Yale" # or TCGA
+    # task_name = "new_PhiHER2_tileAll_twice1000_0abmil_0sel"
+    # task_name = "new_PMIL_Cosine_1insteval_tileAll_twice1000"
+    # task_name = "new_PMIL_Euclidean_1insteval_tileAll_twice1000"
+    # task_name = "CLAM_k8_tileAll"
+    # task_name = "CLAM_k32_tileAll"
+    # task_name = "ABMIL_tileAll"
+    # task_name = "Transformer_tile5k"
+
+
     trained_dataset_name = "HEROHE" # or TCGA
+    task_name = "new_PhiHER2_align_sim_tile5ktwice500"
+    # task_name = "new_ProtoMIL_sim_Cosine_mean_tileAlltwice500_0insteval"
+    # task_name = "new_ProtoMIL_sim_Euclidean_mean_tile5ktwice500_0insteval"
+    # task_name = "new_CLAM_8"
+    # task_name = "new_CLAM"
+    # task_name = "new_ABMIL_tile5k_all"
+    # task_name = "new_Transformer"
     
-    # task_name = "new_PhiTrans_sim_tile5ktwice500_selfqueryparam_normal"
-    # task_name = "new_PhiTrans_sim_tile5ktwice500_selfqueryparam_uniform"
-    # task_name = "new_PhiTrans_sim_tile5ktwice500_query_uniformx4"
-    # task_name = "new_ABMIL"
-    task_name = "new_PhiTrans_align_sim_tile5ktwice500"
-    # task_name = "new_ProtoMIL_sim_Cosine_mean_tile5ktwice500_0insteval"
-    # task_name = "new_ProtoMIL_sim_Cosine_mean"
 
     common_model_dir = "/home/cyyan/Projects/HER2proj/models/"
     model_dir = os.path.join(common_model_dir, trained_dataset_name, "HER2status", task_name)
     print(">>>>>>>>>>>>>>>>>>>{}".format(model_dir))
 
     # params on dataset for indepedent evaluation
-    indepedent_dataset = "HEROHE_test"     # TCGA
+    indepedent_dataset = "TJMUCH70genes"     # TCGA
     common_result_dir = "/home/cyyan/Projects/HER2proj/results/"
     if indepedent_dataset in ["Yale"]:
         feats_path = "".join((common_result_dir, indepedent_dataset, "_2FeatsCCL"))
         csv_path = "/home/cyyan/Projects/HER2proj/data_ModPath_HER2_v3/Yale_HER2status.csv"
+        if indepedent_dataset == trained_dataset_name:
+            csv_path = "/home/cyyan/Projects/HER2proj/results/Yale_3CaseSplits/her2status_KFoldsCV"
+
     elif indepedent_dataset in ["HEROHE_test"]:
         feats_path = "".join((common_result_dir, indepedent_dataset, "_2FeatsCCL_40x"))
         csv_path = "/mnt/DATA/HEROHE_challenge/HEROHE_TestGTinfo.csv"
@@ -181,7 +203,14 @@ if __name__ == "__main__":
     alltimes_summary = []
 
     for tidx in range(params_dict["times"]):
+        os.environ['PYTHONHASHSEED'] = str(params_dict["seed"])
         np.random.seed(params_dict["seed"])
+        torch.manual_seed(params_dict["seed"])
+        if device.type == 'cuda':
+            torch.cuda.manual_seed(params_dict["seed"])
+            torch.cuda.manual_seed_all(params_dict["seed"]) # if you are using multi-GPU.
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
 
         allfold_summary = []
         for kidx in range(params_dict["k"]): # each fold in each time
@@ -193,8 +222,16 @@ if __name__ == "__main__":
             global_cents_feats = prototype_data["global_centroid_feats"]
             
             print("Estimate number of cluster (GLOBAL): {} in fold{} for time {}".format(len(global_cents_feats), kidx, tidx))
+            
             global_cents_feats = global_cents_feats.to(device)
 
+            # 20231224 add for random prototypes evaluation
+            # global_cents_feats = np.random.uniform(size=global_cents_feats.shape, low=global_cents_feats.min(), high=global_cents_feats.max())
+            # global_cents_feats = torch.tensor(global_cents_feats,dtype=torch.float32).to(device)           
+            # global_cents_feats = torch.randn(global_cents_feats.shape).to(device)
+
+            # global_cents_feats = torch.rand_like(global_cents_feats).to(device)
+            
             model = load_model(params_dict, len(global_cents_feats), 
                                trained_pt_loc= os.path.join(model_dir, "time"+str(tidx)),
                                 fold=kidx, device=device)
@@ -206,6 +243,11 @@ if __name__ == "__main__":
             all_preds = []
             all_labels_used = []
             all_cases_embedding = []
+
+            if indepedent_dataset in ["Yale"] and indepedent_dataset == trained_dataset_name:
+                slide_id_list, slide_label_list = load_data_cvfoldtest(csv_path, 
+                                                                       filename=f"splits_time{tidx}_fold{kidx}.csv",
+                                                                       label_mapping={"Neg": 0, "Pos": 1})
             
             with tqdm(total= len(slide_id_list)) as _tqdm: # 使用需要的参数对tqdm进行初始化
                 for sidx, slide_id in enumerate(slide_id_list):
@@ -224,8 +266,8 @@ if __name__ == "__main__":
                     if not os.path.exists(full_path): # 存在个别样本经过s1 tissue seg后没有tissue区域，这个slide不会被用到
                         continue
                     features = torch.load(full_path) # load feats data
-                    # features = features[np.random.choice(len(features), params_dict["num_perslide"]), :] \
-                    #     if params_dict["num_perslide"] is not None and params_dict["num_perslide"] < len(features) else features 
+                    features = features[np.random.choice(len(features), params_dict["num_perslide"]), :] \
+                        if params_dict["num_perslide"] is not None and params_dict["num_perslide"] < len(features) else features 
 
                     with torch.no_grad():
                         logits, Y_prob, Y_hat, _, res_dict = model(features.to(device).type(torch.float32), 
@@ -243,39 +285,54 @@ if __name__ == "__main__":
                                             'pred': Y_hat.cpu().numpy()[0], 'label': slide_label_list[sidx]})
                     
                     _tqdm.update(1)
-                
-                eval_res = pd.concat((pd.DataFrame(patient_results), 
-                                      pd.DataFrame(np.array(all_cases_embedding), columns=[f"sim{i}" for i in  list(range(len(all_cases_embedding[0])))])),
-                                       axis=1)     
-                eval_res.to_csv(os.path.join(model_dir, "time"+str(tidx), f"eval_{indepedent_dataset}_res.csv"))
-                
-                auc = roc_auc_score(all_labels_used, np.array(all_probs)[:, 1])
-                eval_res_dict = classification_report(all_labels_used, np.array(all_preds), 
-                                                 target_names=['neg', 'pos'], output_dict=True)
-                for key, vals in eval_res_dict.items():
-                    if type(vals) is dict:
-                        for sub_key, sub_vals in vals.items():
-                            print(f"{sub_key}: {sub_vals}")
-                    else:
-                        print(f"{key}: {vals}")
-                
-                balanced_acc = balanced_accuracy_score(all_labels_used, np.array(all_preds)) # balanced accuracy is defined as the average of recall obtained on each class.
+            
+            metric_res_dict = {"time": tidx, "fold": kidx}
+            eval_res = pd.concat((pd.DataFrame(patient_results), 
+                                    pd.DataFrame(np.array(all_cases_embedding), columns=[f"sim{i}" for i in  list(range(len(all_cases_embedding[0])))])),
+                                    axis=1)     
+            eval_res.to_csv(os.path.join(model_dir, "time"+str(tidx), f"eval_{indepedent_dataset}_{kidx}_res.csv"))
+            
+            eval_res_dict = classification_report(all_labels_used, np.array(all_preds), 
+                                                target_names=['neg', 'pos'], output_dict=True)
+            for key, vals in eval_res_dict.items():
+                if type(vals) is dict:
+                    for sub_key, sub_vals in vals.items():
+                        print(f"{sub_key}: {sub_vals}")
+                        metric_res_dict[key+"_"+sub_key] = sub_vals
 
-            allfold_summary.append([tidx, kidx, auc, eval_res_dict['pos']['f1-score'], balanced_acc])
+                else:
+                    print(f"{key}: {vals}")
+                    metric_res_dict[key] = vals
+            
+            balanced_acc = balanced_accuracy_score(all_labels_used, np.array(all_preds)) # balanced accuracy is defined as the average of recall obtained on each class.
+            metric_res_dict.update({"balanced acc": balanced_acc})
+            
+            auc = roc_auc_score(all_labels_used, np.array(all_probs)[:, 1])
+            metric_res_dict.update({"auc": auc})
+            
+            allfold_summary.append(metric_res_dict)
 
             print(">>>>>>>>>>>>>>>>>>>Estimate auc value: {} in fold{} for time {}".format(auc, kidx, tidx))
         
-            torch.save({'global_cents_feats': global_cents_feats,
-                        'projection_prototype': res_dict['projection_prototype'], 
-                        'query_prototype': res_dict['query_prototype']
-                        },
-                        os.path.join(model_dir, "time"+str(tidx), f"fold_{kidx}_prototypes_model_projection.pt"))
+            # torch.save({'global_cents_feats': global_cents_feats,
+            #             'projection_prototype': res_dict['projection_prototype'], 
+            #             'query_prototype': res_dict['query_prototype']
+            #             },
+            #             os.path.join(model_dir, "time"+str(tidx), f"fold_{kidx}_prototypes_model_projection.pt"))
 
-        allfold_summary = pd.DataFrame(np.array(allfold_summary), columns=['time', 'fold', 'test_auc', 'f1score', 'bal-acc'])   
+        allfold_summary = pd.DataFrame(allfold_summary)
+        # if kidx > 0:
+        #     allfold_summary.loc['mean'] = allfold_summary.apply(lambda x: x.mean())
+        #     allfold_summary.loc['std'] = allfold_summary.apply(lambda x: x[:-1].std()) # 计算std时 新增的mean行不算在内
+ 
         alltimes_summary.append(allfold_summary)   
 
     alltimes_summary = pd.concat(alltimes_summary)
     alltimes_summary.loc['mean'] = alltimes_summary.apply(lambda x: x.mean())
     alltimes_summary.loc['std'] = alltimes_summary.apply(lambda x: x[:-1].std()) # 计算std时 新增的mean行不算在内
     
+    # os.makedirs(os.path.join(common_model_dir, trained_dataset_name, "HER2status", "randomprototypes_eval_on_PhiHER2"), exist_ok=True)
+    # alltimes_summary.to_csv(os.path.join(common_model_dir, trained_dataset_name, "HER2status", 
+    #                                      "randomprototypes_eval_on_PhiHER2", "summary_metrics_alltimes_kfolds.csv"))
+    alltimes_summary.to_csv(os.path.join(model_dir, f"summary_metrics_alltimes_kfolds_evaluated_{indepedent_dataset}.csv"))    
     print(f'>>>>>>>>>>>>>>>> {indepedent_dataset} DATASET evaluated on TASK {task_name}:\n {alltimes_summary}')
